400791049e008fa9afc96bcb234b9295f1b2c68a
danya-sv <sarapulovdanila@gmail.com>
2024-10-17 18:51:04 +0600

    hw5

---
+++ dz5/main.py
@@ -0,0 +1,10 @@
+from dz5.logic import play_game
+from decouple import config
+
+if __name__ == "__main__":
+    min_number = config('MIN_NUMBER', cast=int)
+    max_number = config('MAX_NUMBER', cast=int)
+    max_attempts = config('MAX_ATTEMPTS', cast=int)
+    initial_capital = config('INITIAL_CAPITAL', cast=int)
+
+    play_game(min_number, max_number, max_attempts, initial_capital)
