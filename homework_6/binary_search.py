104769c4a9162d821be7c0965665c17c79326135
danya-sv <sarapulovdanila@gmail.com>
2024-10-19 20:48:48 +0600

    hw_6

---
+++ homework_6/binary_search.py
@@ -0,0 +1,25 @@
+def binary_search(element, sorted_list):
+    first = 0
+    last = len(sorted_list) - 1
+    result_ok = False
+    pos = -1
+
+    while first <= last:
+        middle = (first + last) // 2
+        if element == sorted_list[middle]:
+            result_ok = True
+            pos = middle
+            break
+        elif element > sorted_list[middle]:
+            first = middle + 1
+        else:
+            last = middle - 1
+
+    if result_ok:
+        print("Элемент найден. Индекс:", pos)
+    else:
+        print("Элемент не найден")
+
+my_list = [2, 4, 6, 8, 10, 12]
+search_element = 6
+binary_search(search_element, my_list)
---
+++ homework_6/bubble_sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(unsorted_list):
+
+  n = len(unsorted_list)
+
+  for i in range(n-1):
+    for j in range(0, n-i-1):
+      if unsorted_list[j] > unsorted_list[j+1] :
+        unsorted_list[j], unsorted_list[j+1] = unsorted_list[j+1], unsorted_list[j]
+  return unsorted_list
+
+my_list = [64, 34, 25, 12, 0, 22, 11, 90, 1000]
+sorted_list = bubble_sort(my_list)
+print("Отсортированный список:", sorted_list)
